6ed095e70f04e3cbd58db0a36468f94f76b4ca65
kk <karkuzhaliomrbranch@gmail.com>
2025-03-15 12:13:26 +0530

    message

---
+++ src/org/com/Third.java
@@ -0,0 +1,15 @@
+package org.com;
+
+public class Third {
+	
+	public void m1() {
+		System.out.println("Greens");
+	}
+	
+	public static void main(String[] args) {
+		
+		Third t = new Third();
+		t.m1();
+	}
+
+}
